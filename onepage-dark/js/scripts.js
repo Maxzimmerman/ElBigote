d9f54abcc00eacc1093f995c8e4dcad9c3af9638
Jesse Hunt <jesse@bembel-webdesign.de>
2023-06-14 12:58:14 +0200

    removed dependencies

--- onepage-dark/js/scripts.js
+++ onepage-dark/js/scripts.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
     "use strict";
-    
+
     // Preloader
     var loader = $(".loader");
     var wHeight = $(window).height();
@@ -26,35 +26,35 @@ $(function() {
             height: '100vh'
         })
     }
-    setTimeout(function() {
+    setTimeout(function () {
         $(".loader-wrapper").fadeOut('fast');
         (loader).fadeOut('fast');
     }, 3500);
-    
+
     var wind = $(window);
     // Main footer 
     var footer = $("footer").outerHeight();
     $("main").css("marginBottom", footer);
     // ScrollIt
     $.scrollIt({
-      upKey: 38,                // key code to navigate to the next section
-      downKey: 40,              // key code to navigate to the previous section
-      easing: 'swing',          // the easing function for animation
-      scrollTime: 600,          // how long (in ms) the animation takes
-      activeClass: 'active',    // class given to the active nav element
-      onPageChange: null,       // function(pageIndex) that is called when page is changed
-      topOffset: -70            // offste (in px) for fixed top navigation
+        upKey: 38,                // key code to navigate to the next section
+        downKey: 40,              // key code to navigate to the previous section
+        easing: 'swing',          // the easing function for animation
+        scrollTime: 600,          // how long (in ms) the animation takes
+        activeClass: 'active',    // class given to the active nav element
+        onPageChange: null,       // function(pageIndex) that is called when page is changed
+        topOffset: -70            // offste (in px) for fixed top navigation
     });
-    
+
     // Navbar scrolling background
-    wind.on("scroll",function () {
+    wind.on("scroll", function () {
         var bodyScroll = wind.scrollTop(),
             navbar = $(".navbar"),
             logo = $(".navbar .logo> img");
-        if(bodyScroll > 100){
+        if (bodyScroll > 100) {
             navbar.addClass("nav-scroll");
             logo.attr('src', 'img/logo-dark.png');
-        }else{
+        } else {
             navbar.removeClass("nav-scroll");
             logo.attr('src', 'img/logo-light.png');
         }
@@ -63,15 +63,15 @@ $(function() {
     $(".navbar-nav .dropdown-item a").on('click', function () {
         $(".navbar-collapse").removeClass("show");
     });
-    
+
     // Sections background image from data background
     var pageSection = $(".bg-img, section");
-    pageSection.each(function(indx){
-        if ($(this).attr("data-background")){
+    pageSection.each(function (indx) {
+        if ($(this).attr("data-background")) {
             $(this).css("background-image", "url(" + $(this).data("background") + ")");
         }
     });
-    
+
     // Animations
     var contentWayPoint = function () {
         var i = 0;
@@ -86,14 +86,11 @@ $(function() {
                             var effect = el.data('animate-effect');
                             if (effect === 'fadeIn') {
                                 el.addClass('fadeIn animated');
-                            }
-                            else if (effect === 'fadeInLeft') {
+                            } else if (effect === 'fadeInLeft') {
                                 el.addClass('fadeInLeft animated');
-                            }
-                            else if (effect === 'fadeInRight') {
+                            } else if (effect === 'fadeInRight') {
                                 el.addClass('fadeInRight animated');
-                            }
-                            else {
+                            } else {
                                 el.addClass('fadeInUp animated');
                             }
                             el.removeClass('item-animate');
@@ -105,15 +102,12 @@ $(function() {
             offset: '85%'
         });
     };
-    
+
     // WayPoint 
     $(function () {
         contentWayPoint();
     });
-    
-    //  YouTubePopUp
-    $("a.vid").YouTubePopUp();
-    
+
     // Team owlCarousel
     $('.team .owl-carousel').owlCarousel({
         loop: true
@@ -125,7 +119,8 @@ $(function() {
         , responsive: {
             0: {
                 items: 1
-            , }
+                ,
+            }
             , 600: {
                 items: 2
             }
@@ -134,7 +129,7 @@ $(function() {
             }
         }
     });
-    
+
     // Testimonials owlCarousel
     $('.testimonials .owl-carousel').owlCarousel({
         loop: true
@@ -146,7 +141,8 @@ $(function() {
         , responsive: {
             0: {
                 items: 1
-            , }
+                ,
+            }
             , 600: {
                 items: 1
             }
@@ -155,7 +151,7 @@ $(function() {
             }
         }
     });
-    
+
     // Menus owlCarousel
     $('.menus .owl-carousel').owlCarousel({
         loop: true
@@ -177,7 +173,7 @@ $(function() {
             }
         }
     });
-    
+
     // Menus Tabs
     $(".menus .tabs-icon").on("click", ".item", function () {
         $(".item").removeClass("active");
@@ -188,27 +184,7 @@ $(function() {
     $(".menus .tabs-icon").on("click", ".owl-item", function () {
         $(this).addClass("actived").siblings().removeClass("actived");
     });
-    
-    // MagnificPopup
-    $(".img-zoom").magnificPopup({
-        type: "image"
-        , closeOnContentClick: !0
-        , mainClass: "mfp-fade"
-        , gallery: {
-            enabled: !0
-            , navigateByImgClick: !0
-            , preload: [0, 1]
-        }
-    })
-    $('.magnific-youtube, .magnific-vimeo, .magnific-custom').magnificPopup({
-        disableOn: 700
-        , type: 'iframe'
-        , mainClass: 'mfp-fade'
-        , removalDelay: 300
-        , preloader: false
-        , fixedContentPos: false
-    });
-    
+
     //  Scroll back to top
     var progressPath = document.querySelector('.progress-wrap path');
     var pathLength = progressPath.getTotalLength();
@@ -236,39 +212,39 @@ $(function() {
     });
     jQuery('.progress-wrap').on('click', function (event) {
         event.preventDefault();
-        jQuery('html, body').animate({ scrollTop: 0 }, duration);
+        jQuery('html, body').animate({scrollTop: 0}, duration);
         return false;
     })
-    
+
 });
 
 // Slider 
-$(document).ready(function() {
+$(document).ready(function () {
     var owl = $('.header .owl-carousel');
     // Slider owlCarousel
     $('.slider .owl-carousel').owlCarousel({
         items: 1,
         dots: false,
-        loop:true,
+        loop: true,
         margin: 0,
         autoplay: true,
-        smartSpeed:500,
-         nav: true,
-         navText: ['<i class="ti-arrow-right" aria-hidden="true"></i>', '<i class="ti-angle-right" aria-hidden="true"></i>']
+        smartSpeed: 500,
+        nav: true,
+        navText: ['<i class="ti-arrow-right" aria-hidden="true"></i>', '<i class="ti-angle-right" aria-hidden="true"></i>']
     });
     // Slider owlCarousel
     $('.slider-fade .owl-carousel').owlCarousel({
         items: 1,
-        loop:true,
+        loop: true,
         dots: false,
         margin: 0,
         autoplay: true,
-        smartSpeed:500,
+        smartSpeed: 500,
         animateOut: 'fadeOut',
         nav: true,
         navText: ['<i class="ti-angle-left" aria-hidden="true"></i>', '<i class="ti-angle-right" aria-hidden="true"></i>']
     });
-    owl.on('changed.owl.carousel', function(event) {
+    owl.on('changed.owl.carousel', function (event) {
         var item = event.item.index - 2;     // Position of the current item
         $('h4').removeClass('animated fadeInUp');
         $('h1').removeClass('animated fadeInUp');
@@ -281,71 +257,42 @@ $(document).ready(function() {
     });
 });
 
-// Mouse effect 
-/*function mousecursor() {
-    if ($("body")) {
-        const e = document.querySelector(".cursor-inner"),
-            t = document.querySelector(".cursor-outer");
-        let n, i = 0,
-            o = !1;
-        window.onmousemove = function (s) {
-            o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
-        }, $("body").on("mouseenter", "a, .cursor-pointer", function () {
-            e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
-        }), $("body").on("mouseleave", "a, .cursor-pointer", function () {
-            $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
-        }), e.style.visibility = "visible", t.style.visibility = "visible"
-    }
-};
-$(function () {
-    mousecursor();
-});*/
-
-// Preloader page
-paceOptions = {
-    ajax: true,
-    document: true,
-    eventLag: false
-};
-
-Pace.on('done', function() {
-  $('#preloader').delay(500).fadeOut(800);
-});
-
-
 
 // Contact Form
-    var form = $('.contact__form'),
-        message = $('.contact__msg'),
-        form_data;
-    // success function
-    function done_func(response) {
-        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
-        message.text(response);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 2000);
-        form.find('input:not([type="submit"]), textarea').val('');
-    }
-    // fail function
-    function fail_func(data) {
-        message.fadeIn().removeClass('alert-success').addClass('alert-success');
-        message.text(data.responseText);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 2000);
-    }
-    form.submit(function (e) {
-        e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
-        })
+var form = $('.contact__form'),
+    message = $('.contact__msg'),
+    form_data;
+
+// success function
+function done_func(response) {
+    message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+    message.text(response);
+    setTimeout(function () {
+        message.fadeOut();
+    }, 2000);
+    form.find('input:not([type="submit"]), textarea').val('');
+}
+
+// fail function
+function fail_func(data) {
+    message.fadeIn().removeClass('alert-success').addClass('alert-success');
+    message.text(data.responseText);
+    setTimeout(function () {
+        message.fadeOut();
+    }, 2000);
+}
+
+form.submit(function (e) {
+    e.preventDefault();
+    form_data = $(this).serialize();
+    $.ajax({
+        type: 'POST',
+        url: form.attr('action'),
+        data: form_data
+    })
         .done(done_func)
         .fail(fail_func);
-    });
+});
     
 
 
